c2cc7daee32e482483905246af7d91add5906f0b
Iga Narendra <iga.narendra@gmail.com>
2025-03-15 13:31:21 +0700

    temporarily disable registration in frontend

--- app/page.tsx
+++ app/page.tsx
@@ -118,10 +118,11 @@ export default function Home() {
                       <Button 
                         variant="outline" 
                         size="lg" 
-                        className="w-full sm:w-auto border-2 border-primary/80 hover:bg-primary hover:text-white text-primary shadow-lg text-base font-semibold px-8 py-6 rounded-md"
+                        className="w-full sm:w-auto border-2 border-primary/80 hover:bg-primary/20 text-primary shadow-lg text-base font-semibold px-8 py-6 rounded-md"
+                        disabled
                       >
                         <Users className="w-5 h-5 mr-2" />
-                        Register Now
+                        Registration Coming Soon
                       </Button>
                     </div>
                   </Link>
